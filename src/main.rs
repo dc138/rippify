71c7cf1eba31c23f358014f277c9ab7106e6731a
DarthChungo <antonio@adaro.eu>
2023-05-10 20:58:00 +0200

    Add colored output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use colored::Colorize;
 use librespot_audio::{AudioDecrypt, AudioFile};
 use librespot_core::{
     authentication::Credentials, config::SessionConfig, session::Session, spotify_id::SpotifyId,
 };
 
-use debug_print::debug_println as dprintln;
 use getopts::Options;
 use lazy_regex::regex;
 use librespot_metadata::{Album, Artist, FileFormat, Metadata, Playlist, Track};
@@ -46,7 +46,7 @@ async fn main() {
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
         Err(f) => {
-            println!("error: {}", f.to_string());
+            println!("{}: {}", "error".red().bold(), f.to_string().bold());
             exit(1);
         }
     };
@@ -73,18 +73,25 @@ async fn main() {
     let user = matches.opt_str("u").unwrap();
     let pass = matches.opt_str("p").unwrap();
 
-    dprintln!("user: {}, pass: {}", &user, &pass);
-
     let credentials = Credentials::with_password(&user, &pass);
     let session_config = SessionConfig::default();
 
     let session = match Session::connect(session_config, credentials, None, false).await {
         Ok(session) => {
-            println!("Logged in as {}", user);
+            println!(
+                "{} {} {}",
+                "=>".green().bold(),
+                "Logged in as:",
+                &user.bright_blue()
+            );
             session.0
         }
         Err(err) => {
-            println!("Cannot log in: {}", err.to_string().to_lowercase());
+            println!(
+                "{}: cannot log in: {}",
+                "error".red().bold(),
+                err.to_string().to_lowercase()
+            );
             exit(1);
         }
     };
@@ -98,26 +105,30 @@ async fn main() {
     let album_uri = regex!(r"^spotify:album:([[:alnum:]]{22})$");
     let album_url = regex!(r"^(http(s)?://)?open\.spotify\.com/album/([[:alnum:]]{22})$");
 
-    println!("Input resources:");
+    println!("\n{} Input resources:", "=>".green().bold());
 
     for line in &input {
         if let Some(captures) = track_uri.captures(&line).or(track_url.captures(&line)) {
             let id_str = captures.iter().last().unwrap().unwrap().as_str();
             let id = SpotifyId::from_base62(&id_str).unwrap();
 
-            println!("  track: {}", &id_str);
+            println!(" {} track: {}", "->".yellow().bold(), &id_str);
 
             track_ids.insert(id);
         } else if let Some(captures) = pl_uri.captures(&line).or(pl_url.captures(&line)) {
             let id_str = captures.iter().last().unwrap().unwrap().as_str();
             let id = SpotifyId::from_base62(&id_str).unwrap();
 
-            println!("  playlist: {}", &id_str);
+            println!(" {} playlist: {}", "->".yellow().bold(), &id_str);
 
             let playlist = match Playlist::get(&session, id).await {
                 Ok(playlist) => playlist,
                 Err(err) => {
-                    println!("    error getting playlist metadata: {:?}, skipping", err);
+                    println!(
+                        "{}: cannot get playlist metadata: {:?}, skipping...",
+                        "warning".yellow(),
+                        err
+                    );
                     continue;
                 }
             };
@@ -129,12 +140,16 @@ async fn main() {
             let id_str = captures.iter().last().unwrap().unwrap().as_str();
             let id = SpotifyId::from_base62(&id_str).unwrap();
 
-            println!("  album: {}", &id_str);
+            println!(" {} album: {}", "->".yellow().bold(), &id_str);
 
             let album = match Album::get(&session, id).await {
                 Ok(album) => album,
                 Err(err) => {
-                    println!("    error getting album metadata: {:?}, skipping", err);
+                    println!(
+                        "{}: cannot get album metadata: {:?}, skipping...",
+                        "warning".yellow(),
+                        err
+                    );
                     continue;
                 }
             };
@@ -143,24 +158,51 @@ async fn main() {
                 track_ids.insert(track);
             }
         } else {
-            println!("  unkown input: {}, skipping", line);
+            println!(
+                "{}: unrecognized input: {}, skipping...",
+                " -> warning".yellow().bold(),
+                line.bold()
+            );
         }
     }
 
     if track_ids.len() == 0 {
-        println!("Didn't get any tracks");
+        println!(
+            "\n{}: didn't get any tracks, aborting...",
+            "error".red().bold()
+        );
         exit(0);
     }
 
-    println!("Parsed {} tracks:", track_ids.len());
+    println!(
+        "\n{} Parsed {} tracks:",
+        "=>".green().bold(),
+        track_ids.len().to_string().bold()
+    );
 
     'track_loop: for track_id in track_ids {
-        println!("  track: {}", track_id.to_base62().unwrap());
+        print!(" {} ", "->".yellow().bold());
 
-        let track = Track::get(&session, track_id).await.unwrap();
+        let track = match Track::get(&session, track_id).await {
+            Ok(track) => {
+                println!("{} ({})", track.name.bold(), track_id.to_base62().unwrap());
+                track
+            }
+            Err(_) => {
+                println!("{} ({})", "??".bold(), track_id.to_base62().unwrap());
+                println!(
+                    "   - {}: cannot get track from id, skipping...",
+                    "warning".yellow().bold(),
+                );
+                continue;
+            }
+        };
 
         if !track.available {
-            println!("    unavailable, skipping");
+            println!(
+                "   - {}: unavailable, skipping...",
+                "warning".yellow().bold()
+            );
             continue;
         }
 
@@ -170,7 +212,8 @@ async fn main() {
                 Ok(artist) => artist,
                 Err(err) => {
                     println!(
-                        "    cannot get artist {} for track: {:?}, skipping...",
+                        "   - {}: cannot get artist {} for track: {:?}, skipping...",
+                        "warning".yellow().bold(),
                         artist_id.to_base62().unwrap(),
                         err
                     );
@@ -180,14 +223,21 @@ async fn main() {
         }
 
         if track_artists.len() == 0 {
-            println!("    cannot get artists for track, skipping...");
+            println!(
+                "   - {}: cannot get artists for track, skipping...",
+                "warning".yellow().bold()
+            );
             continue;
         }
 
         let track_album = match Album::get(&session, track.album).await {
             Ok(album) => album,
             Err(err) => {
-                println!("    cannot get artist for song: {:?}", err);
+                println!(
+                    "   - {}: cannot get artist for song: {:?}",
+                    "warning".yellow().bold(),
+                    err
+                );
                 continue;
             }
         };
@@ -201,7 +251,8 @@ async fn main() {
 
         if Path::new(&track_output_path).exists() {
             println!(
-                "    output file \"{}\" already exists, skipping...",
+                "   - {}: output file \"{}\" already exists, skipping...",
+                "note".bright_blue().bold(),
                 track_output_path
             );
             continue;
@@ -210,7 +261,11 @@ async fn main() {
         let slice_pos = match track_output_path.rfind('/') {
             Some(pos) => pos,
             None => {
-                println!("    invalid format string {}, aborting...", output_format);
+                println!(
+                    "{}: invalid format string {}, aborting...",
+                    "error".red().bold(),
+                    output_format.bold()
+                );
                 exit(1);
             }
         };
@@ -219,7 +274,8 @@ async fn main() {
 
         if create_dir_all(track_folder_path).await.is_err() {
             print!(
-                "    cannot create folders: {}, aborting...",
+                "   - {}: cannot create folders: {}, aborting...",
+                "warning".yellow().bold(),
                 track_folder_path
             );
             exit(1);
@@ -232,11 +288,14 @@ async fn main() {
             .or(track.files.get_key_value(&FileFormat::OGG_VORBIS_96))
         {
             Some(format) => {
-                println!("    using {:?}", format.0);
+                println!("   - using {:?}", format.0);
                 format.1
             }
             None => {
-                println!("    no suitable format found, skipping");
+                println!(
+                    "   - {}: no suitable format found, skipping",
+                    "warning".yellow().bold()
+                );
                 continue;
             }
         };
@@ -244,7 +303,11 @@ async fn main() {
         let track_file_key = match session.audio_key().request(track.id, *track_file_id).await {
             Ok(key) => key,
             Err(err) => {
-                println!("    cannot get audio key: {:?}, skipping", err);
+                println!(
+                    "   - {}: cannot get audio key: {:?}, skipping",
+                    "warning".yellow().bold(),
+                    err
+                );
                 continue;
             }
         };
@@ -252,12 +315,16 @@ async fn main() {
         let mut track_buffer = Vec::<u8>::new();
         let mut track_buffer_decrypted = Vec::<u8>::new();
 
-        println!("    getting encrypted audio file");
+        println!("   - getting encrypted audio file");
 
         let mut track_file_audio = match AudioFile::open(&session, *track_file_id, 40, true).await {
             Ok(audio) => audio,
             Err(err) => {
-                println!("    cannot get audio file: {:?}, skipping", err);
+                println!(
+                    "   - {}: cannot get audio file: {:?}, skipping",
+                    "warning".yellow().bold(),
+                    err
+                );
                 continue;
             }
         };
@@ -266,14 +333,15 @@ async fn main() {
             Ok(_) => {}
             Err(err) => {
                 println!(
-                    "    cannot get track file audio: {}, skipping",
+                    "   - {}: cannot get track file audio: {}, skipping",
+                    "warning".yellow().bold(),
                     err.to_string()
                 );
                 continue;
             }
         };
 
-        println!("    decrypting audio");
+        println!("   - decrypting audio");
 
         match AudioDecrypt::new(track_file_key, &track_buffer[..])
             .read_to_end(&mut track_buffer_decrypted)
@@ -281,14 +349,15 @@ async fn main() {
             Ok(_) => {}
             Err(err) => {
                 println!(
-                    "    cannot decrypt audio file: {}, skipping",
+                    "   - {}: cannot decrypt audio file: {}, skipping",
+                    "warning".yellow().bold(),
                     err.to_string()
                 );
                 continue;
             }
         };
 
-        println!("    writing tags");
+        println!("   - writing output file");
 
         let track_file_cursor = Cursor::new(&track_buffer_decrypted[0xa7..]);
         let mut track_comments = CommentHeader::new();
@@ -304,16 +373,15 @@ async fn main() {
 
         let mut track_file_out = replace_comment_header(track_file_cursor, track_comments);
 
-        println!("    writing output file");
-
         let mut track_file_write = File::create(&track_output_path).await.unwrap();
         match copy(&mut track_file_out, &mut track_file_write).await {
             Ok(_) => {
-                println!("    wrote \"{}\"", track_output_path);
+                println!("   - wrote \"{}\"", track_output_path);
             }
             Err(err) => {
                 println!(
-                    "    cannot write {}: {}, skipping",
+                    "   - {}: cannot write {}: {}, skipping...",
+                    "warning".yellow().bold(),
                     track_output_path,
                     err.to_string()
                 );
